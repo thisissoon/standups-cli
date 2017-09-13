9966c7e678547a8e7944f166a1dc90221030941c
Jack Matthews <jackhkmatthews@gmail.com>
2017-09-13 17:52:09 +0100

    create function

--- http/staff-member.js
+++ http/staff-member.js
@@ -4,5 +4,19 @@ function list() {
   return got('http://localhost:3000/v1/staff-members?sort=firstName:asc', { json: true });
 }
 
-module.exports = {list};
+function create(staffMember) {
+  return got.post('http://localhost:3000/v1/staff-members', {
+    json: true,
+    body: staffMember
+  })
+  .then(response => {
+    staffMember.staffID = response.body._links.staffMember.href.split('/').pop();
+    return staffMember;
+  })
+  .catch(err => {
+    throw new Error('staff member could not be saved');
+  });
+}
+
+module.exports = {list, create};
 
